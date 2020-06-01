850ccce55c8f2f96f32562a4882b5c433e362344
Huang Minzhi <m_fan@geishatokyo.com>
2020-06-01 16:39:56 +0900

    update particle transform

--- src/assets/vertexshader.glsl
+++ src/assets/vertexshader.glsl
@@ -14,16 +14,27 @@ void main() {
   #include <begin_vertex>
   float v0 = 1.5;
   float a = 1.5;
+  float vy0 = 1.5;
+  float vy1 = -1.0;
+  float ay = -2.0;
+  float vy = vy0 + time * ay;
   float v = v0 - time * a;
   float d = 0.0;
+  float dy = 0.0;
   if (v > 0.0){
     d = (v + v0) * time * 0.5;
   }else{
     d = v0 * v0 / a * 0.5;
   }
+  if (vy > vy1){
+    dy = vy0 * time + 0.5 * ay * time * time;
+  } else {
+    float d0 = (vy1 * vy1 - vy0 * vy0) / ay * 0.5;
+    dy = d0 + vy1 * (time - (vy1 - vy0) / ay);
+  }
   transformed.x = transformed.x + position.x * d;
   transformed.z = transformed.z + position.z * d;
-  transformed.y = transformed.y -(0.25 + 0.4* rand(position.xy))*time;
+  transformed.y = transformed.y + dy;
   // if (transformed.y < -2.0) transformed.y = -2.0;
   #include <morphtarget_vertex>
   #include <project_vertex>
